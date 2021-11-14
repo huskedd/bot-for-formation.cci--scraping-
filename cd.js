ec37a3fec23c846323d1984ff748f6dbfc3b4f90
huskedd <60286388+huskedd@users.noreply.github.com>
2021-11-14 16:41:18 +0100

    added intents and new functions

--- cd.js
+++ cd.js
@@ -1,11 +1,15 @@
 const puppeteer = require('puppeteer');
-const Discord = require('discord.js');
-const client = new Discord.Client();
 const usrData = require('./userpass.json');
-const token = require('./bot.json');
+const data = require('./bot.json');
+const Discord = require('discord.js'); //import discord.js
+require('discord-reply');
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
 
-client.login(bot.token);
+
+
+
 
 //!!!!! warining data clear !!!!!! when printing out userpass.json
 console.log(usrData);
@@ -19,7 +23,7 @@ function delay(time) {
  };
 
 
-(async () => {
+const fetchEdt = (async () => {
     
     const browser = await puppeteer.launch({headless:false});
     console.log("browser launched");
@@ -31,17 +35,17 @@ function delay(time) {
     console.log("connection au compte");
     await page.type('#login', usrData.username);
     await page.type('#password', usrData.password);
-    url = "https://formations.cci-paris-idf.fr/UTEC/index.php/apprenant/planning/courant/";
+    url = "https://formations.cci-paris-idf.fr/UTEC/index.php/apprenant/planning/courant/?semaineDebut=202146&ressources=YToxOntpOjA7YToyOntzOjQ6ImNvZGUiO2k6MzcwOTk2NjtzOjQ6InR5cGUiO2k6NzUwMDt9fQ==&modeAffichage=0";
 
 
     await page.click('#btnSeConnecter');
     await console.log("connected successfully !");
     await console.log("4s cooldown");
-    await page.waitForTimeout(6000);
+    await page.waitForTimeout(4000);
     console.log("wait 10 second until the page load");
         for(i = 0; i <11; i++){
         await page.waitForTimeout(1000)
-            console.log(i);
+            console.log( ": "+ i);
     }
 
 
@@ -61,12 +65,16 @@ function delay(time) {
 
 
     //console.log("envoie de l'emploi du temps sur discord dans le channel (nom du channel")
-})();
+});
 
+function sendEdt(){
+    const SendingToDiscord = new Discord.MessageAttachment('https://randomdev.fr/wp-content/uploads/2020/05/IMG_2918-scaled-e1588871523945-932x1024.jpeg');
+}
 
-client.on("message", msg => {
-    if (msg.content === "!edt"){
-        msg.reply("salut");
-    }
-})
+client.on('ready', () => {
+    fetchEdt()
+    sendEdt()
+        
+  });
 
+  client.login(data.token);
